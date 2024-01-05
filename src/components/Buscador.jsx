d02dbc527ab3879e9c1a0ae2197acdc2d803271d
Marie <d100403m@gmail.com>
2024-01-05 19:07:47 -0300

    commit

---
+++ src/components/Buscador.jsx
@@ -0,0 +1,9 @@
+const Buscador = () => {
+    return <div class="px-5 mt-5">
+        <h2>Buscar</h2>
+                <label class="form-label" htmlFor="inputTitle">Título</label>
+                <input class="form-control" type="text" id="inputTitle"/>
+    </div>;
+}
+
+export default Buscador;
